746153a1098880893f40c0251278fbce7035ed9b
Olehandro767 <oleg.03.07.2000@gmail.com>
2022-03-07 18:35:26 +0200

    init

---
+++ src/main/kotlin/ua/spring/dataJPACP/SpringDataJpaControlPanelApplication.kt
@@ -0,0 +1,11 @@
+package ua.spring.dataJPACP
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaControlPanelApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDataJpaControlPanelApplication>(*args)
+}
---
+++ src/test/kotlin/ua/spring/dataJPACP/SpringDataJpaControlPanelApplicationTests.kt
@@ -0,0 +1,12 @@
+package ua.spring.dataJPACP
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaControlPanelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
